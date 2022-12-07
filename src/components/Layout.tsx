22c633345a9daa03d7d17f246c2e11720731fbb1
japoodev <jafarsaadique@gmail.com>
2022-12-07 20:30:42 +0530

    Fix: mobile height overflow

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,15 +7,15 @@ type LayoutProps = {
 
 const Layout = ({children}: LayoutProps) => {
     return (
-        <div>
-            <div className="flex flex-col min-h-screen">
+        <>
+            <div className="flex flex-col h-full">
                 <Header />
                 <main className="flex-1 flex flex-col justify-center items-center">
                     {children}
                 </main>
                 <Footer />
             </div>
-        </div>
+        </>
     )
 }
 
